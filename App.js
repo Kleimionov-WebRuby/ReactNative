c5299650eaacfa627a7149c153286e5587a53b10
Kleimionov-WebRuby <kleymonovweb@gmail.com>
2022-10-07 10:04:29 +0300

    Remove unnecessary files

--- App.js
+++ App.js
@@ -1,64 +1,12 @@
-import { useState } from 'react';
-import { StyleSheet, ImageBackground, SafeAreaView, StatusBar } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-
-import StartGameScreen from './Screens/StartGameScreen';
-import GameScreen from './Screens/GameScreen';
-import GameOverScreen from './Screens/GameOverScreen';
-import Colors from './constants/colors';
+import { StyleSheet, View, Text, SafeAreaView } from 'react-native';
 
 export default function App() {
-  const [userNumber, setUserNumber] = useState();
-  const [gameIsOver, setGameIsOver] = useState(false);
-  const [guessRoundsNumber, setGuessRoundsNumber] = useState(0);
-
-  const pickedNumberHandler = pickedNumber => setUserNumber(pickedNumber);
-
-  const gameOverHandler = numberOfRounds => {
-    setGameIsOver(true);
-    setGuessRoundsNumber(numberOfRounds);
-  };
-
-  const newGameStartHandler = () => {
-    setUserNumber(null);
-    setGameIsOver(false);
-    setGuessRoundsNumber(0);
-  };
-
-  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
-
-  if (userNumber) {
-    screen = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />;
-  }
-
-  if (gameIsOver && userNumber) {
-    screen = (
-      <GameOverScreen
-        userNumber={userNumber}
-        roundsNumber={guessRoundsNumber}
-        onStartNewGame={newGameStartHandler}
-      />
-    );
-  }
-
   return (
-    <>
-      <StatusBar style="light" />
-      <LinearGradient
-        style={styles.rootScreen}
-        colors={[Colors.primary500, Colors.accent500]}
-        locations={[0.4, 1]}
-      >
-        <ImageBackground
-          source={require('./assets/images/background.png')}
-          resizeMode="cover"
-          style={styles.rootScreen}
-          imageStyle={styles.backgroundImage}
-        >
-          <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
-        </ImageBackground>
-      </LinearGradient>
-    </>
+    <SafeAreaView style={styles.rootScreen}>
+      <View>
+        <Text>Hello Navigation</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -66,7 +14,5 @@ const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
   },
-  backgroundImage: {
-    opacity: 0.15,
-  },
+  container: {},
 });
